2a24edff7c53ba0d0ae436acf05340c35a5e31db
Mohanasankari D <mohana280706dvsm@gmail.com>
2026-08-01 16:17:37 +0530

    Time: 1 ms (96.63%) | Memory: 42.8 MB (5.30%) - LeetSync

---
+++ 231-power-of-two/power-of-two.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+    
+    
+    for(int i=0;i<=30;i++)
+    {
+        if(n==Math.pow(2,i))
+        {
+            return true;
+        }
+    }
+      return false;  
+    }
+}
